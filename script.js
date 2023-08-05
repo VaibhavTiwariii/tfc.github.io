0f11f3b9462add62a826bf247e6d5575aa3aa800
VaibhavTiwariii <138752219+VaibhavTiwariii@users.noreply.github.com>
2023-08-05 12:27:14 +0530

    another commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+document.addEventListener("mousemove",function (dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top =  dets.y+"px"
+    blur.style.left = dets.x-100+"px"
+    blur.style.top =  dets.y-100+"px"
+
+});
+
+gsap.to("#nav", {
+    backgroundColor: "#000",
+    duration: 0.5,
+    height: "100px",
+    scrollTrigger: {
+      trigger: "#nav",
+      scroller: "body",
+      // markers:true,
+      start: "top -10%",
+      end: "top -11%",
+      scrub: 1,
+    }
+  })
+  gsap.to("#main",{
+    backgroundColor: "#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scoller:"body",
+        // markers:true
+        start:"top -25%",
+        end:"top -75%",
+        scrub:2
+    }
+
+  })
+  
